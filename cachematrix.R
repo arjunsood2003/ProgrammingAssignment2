fdbb63c238ffccb42945755157a35341494dbe7d
arjunsood2003 <arjunsood2003@gmail.com>
2017-05-21 16:11:47 +0530

    Caching matrix inversing using R
    Coursera course on R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions help construct a square matrix object with its inverse  
+## with getters/setters, calculating the inverse only if it hasn't been calculated yet.
+## Else, cached data is retrieved.
 
-## Write a short comment describing this function
+## makeCacheMatrix has a square matrix object 'x' and its inverse 'i'
+## Getter/Setter functions help to get/set the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(invers) i <<- invers
+        getinv <- function() i
+        list(set = set, get = get, getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve retries the square matrix x's inverse if calculated earlier already.
+## Else, it calculates it and then stores it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)){
+                message("Getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data,...)
+        x$setinv(i)
+        i
 }
